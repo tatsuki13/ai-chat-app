7fff08fc2c108fa7b82791c21b3f8d0546038b90
tatsuki13 <furuno.tatsuki520@mail.kyutech.jp>
2026-04-23 15:38:51 +0900

    画面の表示に成功

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/SkUqmuEhOBtlIWALILmRWtuZKEtvDqfdh1tzW1c4qA=\"\n}"
---
+++ app/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>AIチャットアプリ開発開始！</h1>;
+}
